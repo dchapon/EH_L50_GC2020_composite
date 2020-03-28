d41fe0dd87f7be3d0d2463481c4fceef5a1feec4
Damien CHAPON <damien.chapon@cea.fr>
2020-03-28 22:05:55 +0100

    Added density/temperature image composition example in ipython notebook

---
+++ misc.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import numpy as N
+from PIL import Image
+from matplotlib import pyplot as P
+
+
+def value_range(arr, drop_percentile=1.0):
+    values = N.sort(arr[arr > 0.0])
+    weights = (values - values[0]) / (values[-1] - values[0])
+    cumval = N.cumsum(weights)
+    cumval /= cumval[-1]
+    mask = (cumval >= drop_percentile/100.0)
+    vmin = values[mask][0]
+    return vmin, values[-1]
+
+
+def pil_img(a, lamin, cmap_name):
+    cmap = P.cm.get_cmap(cmap_name)
+    la = N.log10(a.T)
+    la[la<lamin] = lamin
+    lamax = la.max()
+    a_arr = (la-lamin)/(lamax-lamin)
+    ima_arr = N.clip(cmap(a_arr)*255.0, 0, 255).astype('uint8')
+    ima = Image.fromarray(ima_arr[:, :, :3], "RGB").transpose(Image.FLIP_TOP_BOTTOM)
+    return ima
+
+
+__all__ = ["value_range", "pil_img"]
